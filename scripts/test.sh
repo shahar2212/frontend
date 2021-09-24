ee47ff210d1d56859a468988a44f2905e497512d
shahar <s@010.co.il>
2021-09-24 17:26:35 +0300

    Add 'Test' stage

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,4 @@
-!/usr/bin/env sh
-
-set -x
+#!/usr/bin/env sh
 
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
@@ -11,16 +9,12 @@ echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
-
-
-
-npm install --save-dev cross-env
-
+set -x
+# npm install --save-dev cross-env
 set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
-
-
+set -x
 npm test
